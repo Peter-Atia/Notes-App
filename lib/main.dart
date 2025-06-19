331db52b932f2bcb0db80af4f6bc9e2866345b92
Peter-Atia <potrosatia1@gmail.com>
2025-06-19 13:15:19 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("data"),
+      ),
+    );
+  }
+}
